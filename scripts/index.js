a05c2b0e8eac0d1ed9a0176f96eb4d77ae4932d5
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-10-07 22:46:34 +0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,459 @@
+
+const experienceMapping = {
+  "ledgerise":  {
+    title: "Founder @ Ledgerise | 2 years",
+    description: "100% transparent and trustworthy proof-of-donation-delivery platform",
+    paragraph: "After the 6 February 2023 earthquake in Turkey, I saw the transparency problem in donation campaigns in Turkey. Then I started Ledgerise to solve this problem, bring transparency and trustworthiness to donation campaigns. The platform 100% proves that donation of the donors meet the beneficiary. Ledgerise currently operates with 7 collaborative organizations, 100+ monthly donation parcel traffic and 1000+ beneficiary.",
+    roles: [
+      "Head of a team of 3 from Istanbul Technical University.",
+      "Launched LR Suite, with over 22,000 lines of code.",
+      "Completed 8 month delivery software R&D.",
+      "Supported by ITU Seed, #1 start-up incubator in Europe.",
+      "12+ news on innovative technologies media.",
+      "Youngest founder supported by ITU Çekirdek (at age 17).",
+    ],
+    skills: [
+      "Blockchain development",
+      "AI development",
+      "Fullstack development",
+      "Mobile development",
+      "Computer vision development",
+      "Leadership & Marketing", 
+      "UI/UX Design"
+    ],
+    galleryWrapper: [
+      {
+        src: "/public/experience/ledgerise1.png",
+        description: "Interview at SosyalUp Magazine",
+        datetime: "Turkey's largest social entrepreneurship magazine."
+      },
+      {
+        src: "/public/experience/ledgerise2.png",
+        description: "Me at ITU Çekirdek pitching to start-up ecosystem of Turkey.",
+        datetime: "17 August 2024 | ITU ARI Technopark Maslak"
+      },
+      {
+        src: "/public/experience/ledgerise3.png",
+        description: "Pitching to geographical technology juries",
+        datetime: "Science fair at my high school."
+      },
+      {
+        src: "/public/experience/ledgerise4.png",
+        description: "Pitching Ledgerise to over 50 investors",
+        datetime: "18 September 2024 | IS Bank Towers Istanbul"
+      }
+    ]
+  },
+  "gonullubul":  {
+    title: "Co-Founder @ Gonullubul.org | 3 years",
+    description: "First online volunteering social media for high school students in Turkey.",
+    paragraph: "I and my co-founder Hande, started gonullubul to help our friends find social outreach projects. The problem was that the school requires 15 hours of social service per year, but we couldn't find a project to actually attend. We actively solve this problem with gonullubul.org.",
+    roles: [
+      "Responsible for all the software development and maintenance.",
+      "Launched the app, with over 11,000 lines of code.",
+      "Managed a team of 3 software developers.",
+      "Currently live with 5 NGOs, over 100 high school active volunteers.",
+    ],
+    skills: [
+      "Leadership",
+      "Fullstack development",
+      "Backend development",
+      "Frontend development",
+      "Team management",
+      "UI/UX Design"
+    ],
+    galleryWrapper: [
+      {
+        src: "/public/experience/gonullubul1.png",
+        description: "Progress Page",
+        datetime: "gonullubul.org"
+      },
+      {
+        src: "/public/experience/gonullubul2.png",
+        description: "Home Page",
+        datetime: "gonullubul.org"
+      },
+      {
+        src: "/public/experience/gonullubul3.png",
+        description: "Project Page",
+        datetime: "gonullubul.org"
+      },
+    ]
+  },
+  "peertutoring":  {
+    title: "Founder @ Peertutoring.net | 6 months",
+    description: "First online peer tutoring platform for high school students in Turkey",
+    paragraph: "I started peer tutoring after I notice the problems in the execution of peer tutoring system in our school. The data was stored in multiple places, teachers weren't able to filter data about the program... I solved this problem by launching an app that all mentoring programs can be operated in.",
+    roles: [
+      "Responsible for all the software development and maintenance.",
+      "Launched the app, with over 4,000 lines of code.",
+      "Currently live in Uskudar American Academy, starting in 2024 fall.",
+    ],
+    skills: [
+      "Fullstack development",
+      "Backend development",
+      "Frontend development",
+      "UI/UX Design"
+    ],
+    galleryWrapper: [
+      {
+        src: "/public/experience/peertutoring1.png",
+        description: "Calendar Page",
+        datetime: "peertutoring.net"
+      },
+      {
+        src: "/public/experience/peertutoring2.png",
+        description: "Student Page",
+        datetime: "peertutoring.net"
+      },
+      {
+        src: "/public/experience/peertutoring3.png",
+        description: "Mentor Page",
+        datetime: "peertutoring.net"
+      },
+    ]
+  },
+
+  "usersmagic":  {
+    title: "Software Developer @ Usersmagic | 10 months",
+    description: "CRM analytics software for companies",
+    paragraph: "Usersmagic is a USA based data analysis start-up. We help companies to publish surveys and get insighful data from their users. I started working as a full-stack developer at 15 years old and launched multiple websites.",
+    roles: [
+      "Usersmagic worked with top class companies: Nike, Teknosa, Marketyo, Lufian and Varan.",
+      "After I joined we increased our users from 120K to 170K in 3 months.",
+      "Launched our graphing data analysis engine for companies.",
+      "Generated weekly reports and case studies.",
+      "Worked on landing pages.",
+      "Worked on tester page interactive design.",
+    ],
+    skills: [
+      "Fullstack development",
+      "Backend development",
+      "Frontend development",
+      "UI/UX Design"
+    ],
+    galleryWrapper: [
+      {
+        src: "/public/experience/usersmagic1.png",
+        description: "Interactive Interface",
+        datetime: "Worked on interactive, user-friendly design mechanisms."
+      },
+      {
+        src: "/public/experience/usersmagic2.png",
+        description: "Testing software",
+        datetime: "Worked on testing software interface."
+      },{
+        src: "/public/experience/usersmagic3.png",
+        description: "Landing page",
+        datetime: "Worked on complex adaptive designs."
+      },
+    ]
+  },
+
+  "kadirhas":  {
+    title: "Teaching Assitant @ Kadir Has University Summer School | 2 months",
+    description: "Quantum Mechanics (Aug 2023) | Electricity & Magnetism (Aug 2024)",
+    paragraph: "After taking the summer schools courses at Kadir Has University, selected as teaching assistant by MIT Emeritus Professor Nihat Berker. Assisted teaching to my alotted students.",
+    roles: [
+      "Marked quizzes and daily homeworks.",
+      "Teached topics like infinite potential well, Heisenberg uncertainty principle, Schrödinger's equaton and Tunneling.",
+      "Teached topics like infinite Coulomb's Law, Faraday's laws, Maxwell's equations.",
+    ],
+    skills: [
+      "Quantum Mechanics",
+      "Electricity and Magnetism",
+      "Teaching",
+      "Communication"
+    ],
+    galleryWrapper: [
+      {
+        src: "/public/experience/kadirhas1.png",
+        description: "Certificate 1",
+        datetime: "Quantum Mechanics"
+      },
+      {
+        src: "/public/experience/kadirhas2.png",
+        description: "Certificate 2",
+        datetime: "Electricity and Magnetism"
+      },
+    ]
+  }
+};
+
+window.onload = () => {
+  
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("each-gallery-thumbnail-img")) {
+
+      if (document.getElementById("gallery-detail-wrapper")) {
+        document.getElementById("gallery-detail-wrapper").remove();
+      }
+
+      const galleryDetailWrapper = document.createElement("div");
+      galleryDetailWrapper.classList.add("gallery-detail-wrapper");
+      galleryDetailWrapper.id = "gallery-detail-wrapper";
+      
+
+      const detailImageContent = event.target.parentNode.children[0];
+      const detailDescriptionContent = event.target.parentNode.children[1];
+      const detailDatetimeContent = event.target.parentNode.children[2];
+
+      const galleryDetailImage = document.createElement("img");
+      galleryDetailImage.src = detailImageContent.src;
+      galleryDetailImage.classList.add("gallery-detail-image");
+
+      const galleryDetailDescription = document.createElement("div");
+      galleryDetailDescription.innerHTML = detailDescriptionContent.innerHTML;
+      galleryDetailDescription.style.color = "#eee";
+
+      const galleryDetailDateTime = document.createElement("div");
+      galleryDetailDateTime.innerHTML = detailDatetimeContent.innerHTML;
+      galleryDetailDateTime.style.fontSize = "14px";      
+      galleryDetailDateTime.style.color = "#ccc";
+
+
+      const galleryDetailTextWrapper = document.createElement("div");
+      galleryDetailTextWrapper.appendChild(galleryDetailDescription);
+      galleryDetailTextWrapper.appendChild(galleryDetailDateTime);
+
+      galleryDetailTextWrapper.classList.add("gallery-detail-text-wrapper");
+
+      galleryDetailWrapper.appendChild(galleryDetailImage);
+      galleryDetailWrapper.appendChild(galleryDetailTextWrapper);
+
+
+      const galleryDetailCloseButton = document.createElement("div");
+      galleryDetailCloseButton.classList.add("gallery-detail-close-button");
+      galleryDetailCloseButton.innerHTML = "×"
+
+      galleryDetailWrapper.appendChild(galleryDetailCloseButton);
+      galleryDetailCloseButton.addEventListener("click", (event) => {
+        event.target.parentNode.remove();
+      })
+
+      document.body.appendChild(galleryDetailWrapper);
+    
+    }
+
+    else if (event.target.classList.contains("each-slot-selection-content") || event.target.parentNode.classList.contains("each-slot-selection-content")) {
+      const experienceRightWrapper = document.getElementById("each-subtitle-slot-wrapper-right");
+
+      const id = event.target.classList.contains("each-slot-selection-content") ? event.target.id : event.target.parentNode.classList.contains("each-slot-selection-content") ? event.target.parentNode.id : "";
+
+      experienceRightWrapper.style.opacity = "0";
+      
+
+      // Create the main wrapper div
+      setTimeout(() => {
+        experienceRightWrapper.innerHTML = "";
+        experienceRightWrapper.style.opacity = "1"
+
+
+      // Create and append the h1 title
+      const mainTitle = document.createElement('h1');
+      mainTitle.className = '';
+      mainTitle.id = 'experience-main-title';
+      mainTitle.textContent = experienceMapping[id].title;
+      experienceRightWrapper.appendChild(mainTitle);
+
+      // Create and append the h3 description
+      const mainDescription = document.createElement('h3');
+      mainDescription.className = '';
+      mainDescription.id = 'experience-main-description';
+      mainDescription.textContent = experienceMapping[id].description;
+      experienceRightWrapper.appendChild(mainDescription);
+
+      // Create and append the paragraph description
+      const paragraph = document.createElement('p');
+      paragraph.textContent = experienceMapping[id].paragraph;
+      experienceRightWrapper.appendChild(paragraph);
+
+      // Create and append the unordered list
+      const list = document.createElement('ul');
+      const listItems = experienceMapping[id].roles;
+
+      listItems.forEach(itemText => {
+          const listItem = document.createElement('li');
+          listItem.textContent = itemText;
+          list.appendChild(listItem);
+      });
+      experienceRightWrapper.appendChild(list);
+
+      // Create the experiences main wrapper
+      const experiencesWrapper = document.createElement('div');
+      experiencesWrapper.className = 'experiences-main-wrapper';
+      experienceRightWrapper.appendChild(experiencesWrapper);
+
+      // Create and append skills section
+      const skillsDiv = document.createElement('div');
+      skillsDiv.textContent = 'Skills';
+      experiencesWrapper.appendChild(skillsDiv);
+
+      const skillsWrapper = document.createElement('div');
+      skillsWrapper.className = 'experience-skills-wrapper';
+      skillsWrapper.id = 'experience-skills-wrapper';
+
+      const skills = experienceMapping[id].skills;
+
+      skills.forEach(skill => {
+          const skillDiv = document.createElement('div');
+          skillDiv.className = 'each-experience-content';
+          skillDiv.id = 'each-experience-content';
+          skillDiv.textContent = skill;
+          skillsWrapper.appendChild(skillDiv);
+      });
+      experiencesWrapper.appendChild(skillsWrapper);
+
+      // Create and append the gallery wrapper
+      const galleryWrapper = document.createElement('div');
+      galleryWrapper.className = 'gallery-main-wrapper';
+      experienceRightWrapper.appendChild(galleryWrapper);
+
+      const galleryTitle = document.createElement('h3');
+      galleryTitle.textContent = 'Gallery';
+      galleryWrapper.appendChild(galleryTitle);
+
+      const galleryContentWrapper = document.createElement('div');
+      galleryContentWrapper.className = 'gallery-main-content-wrapper';
+      galleryWrapper.appendChild(galleryContentWrapper);
+
+      // Create gallery thumbnail section
+      const thumbnailWrapper = document.createElement('div');
+      thumbnailWrapper.className = 'gallery-photo-thumbnails-wrapper';
+
+      experienceMapping[id].galleryWrapper.forEach(eachPhoto => {
+
+        const galleryThumbnail = document.createElement('div');
+        galleryThumbnail.className = 'each-gallery-thumbnail';  
+        
+        const thumbnailImg = document.createElement('img');
+        thumbnailImg.className = 'each-gallery-thumbnail-img';
+        thumbnailImg.src = eachPhoto.src;
+        galleryThumbnail.appendChild(thumbnailImg);
+
+        // Add description and datetime
+        const thumbnailDescription = document.createElement('div');
+        thumbnailDescription.className = 'each-gallery-slot-description';
+        thumbnailDescription.textContent = eachPhoto.description;
+        galleryThumbnail.appendChild(thumbnailDescription);
+
+        const thumbnailDatetime = document.createElement('div');
+        thumbnailDatetime.className = 'each-gallery-slot-datetime-wrapper';
+        thumbnailDatetime.textContent = eachPhoto.datetime;
+        galleryThumbnail.appendChild(thumbnailDatetime);
+
+        thumbnailWrapper.appendChild(galleryThumbnail);
+        galleryContentWrapper.appendChild(thumbnailWrapper);
+
+      })
+
+      // Create arrows for gallery navigation
+      const galleryArrowsWrapper = document.createElement('div');
+      galleryArrowsWrapper.className = 'gallery-main-arrows-wrapper';
+
+      const leftArrow = document.createElement('div');
+      leftArrow.className = 'each-arrow-content';
+      leftArrow.textContent = '←';
+
+      const rightArrow = document.createElement('div');
+      rightArrow.className = 'each-arrow-content';
+      rightArrow.textContent = '→';
+
+      galleryArrowsWrapper.appendChild(leftArrow);
+      galleryArrowsWrapper.appendChild(rightArrow);
+      galleryContentWrapper.appendChild(galleryArrowsWrapper);
+    }, 1000);
+    }
+  })
+
+
+
+  const displayGalleryPhotosWrapper = document.getElementById("display-gallery-photos-wrapper");
+
+  const displayGalleryPhotosContentArray = [
+    {
+      title: "Friday night, after a long day",
+      description: "After worldef e-commerce forum.",
+      src: "/public/gallery/sunset1.png"
+    },
+    {
+      title: "Lost in Istanbul",
+      description: "Bosphorus bridge on the background.",
+      src: "/public/gallery/bosphorus.png"
+    },
+    {
+      title: "Connecting with the nature",
+      description: "At the village house's cozy coffee shop.",
+      src: "/public/gallery/village1.png"
+    },
+    {
+      title: "After a busy day",
+      description: "After worldef e-commerce forum.",
+      src: "/public/gallery/sunset2.png"
+    },
+    {
+      title: "At the fitness gym",
+      description: "Weekend shoulder day.",
+      src: "/public/gallery/fitness1.png"
+    },
+    {
+      title: "Born in the village.",
+      description: "Revival after a long city day.",
+      src: "/public/gallery/village2.png"
+    },
+    {
+      title: "After a tiring Tango night.",
+      description: "Dance dance dance 💃💃💃",
+      src: "/public/gallery/mirror.png"
+    },
+    {
+      title: "At the fitness gym",
+      description: "Friday Night Leg Day",
+      src: "/public/gallery/fitness2.png"
+    },
+    {
+      title: "Pergamon Antique City",
+      description: "Where people heal with the sound of water.",
+      src: "/public/gallery/aspendos.png"
+    }
+  ];
+
+  displayGalleryPhotosContentArray.forEach(eachDisplay => {
+    const eachPhoto = document.createElement("div");
+    eachPhoto.classList.add("display-gallery-each-photo");
+    const eachPhotoImg = document.createElement("img");
+    eachPhotoImg.src = eachDisplay.src;
+
+    eachPhoto.appendChild(eachPhotoImg);
+
+    displayGalleryPhotosWrapper.appendChild(eachPhoto);
+
+
+    eachPhoto.addEventListener("mouseenter", (event) => {
+      
+      eachPhoto.style.scale = "1.2";
+      const displayGalleryTextWrapper = document.createElement("div");
+      
+      const displayGalleryTextTitle = document.createElement("div");
+      displayGalleryTextTitle.innerHTML = eachDisplay.title;
+      displayGalleryTextTitle.style.fontSize = "18px";
+      displayGalleryTextTitle.style.marginBottom = "10px";
+
+      const displayGalleryTextDescription = document.createElement("div");
+      displayGalleryTextDescription.innerHTML = eachDisplay.description;
+      displayGalleryTextDescription.style.fontSize = "14px";
+
+      displayGalleryTextWrapper.appendChild(displayGalleryTextTitle);
+      displayGalleryTextWrapper.appendChild(displayGalleryTextDescription);
+
+      displayGalleryTextWrapper.classList.add("display-gallery-text-wrapper");
+
+      eachPhoto.appendChild(displayGalleryTextWrapper);
+
+      eachPhoto.addEventListener("mouseleave", (event) => {
+        eachPhoto.style.scale = "1";
+        displayGalleryTextWrapper.remove();
+      });
+    })
+  })
+}
